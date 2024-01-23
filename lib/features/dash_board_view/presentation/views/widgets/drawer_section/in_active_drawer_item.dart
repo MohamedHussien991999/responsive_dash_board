2b2dff1bbfaa4369cf29c043e69d6008a062122a
Mohamed Hussien <93053169+MohamedHussien991999@users.noreply.github.com>
2024-01-23 13:15:43 +0200

    Responsive tablet layout done

--- lib/features/dash_board_view/presentation/views/widgets/drawer_section/in_active_drawer_item.dart
+++ lib/features/dash_board_view/presentation/views/widgets/drawer_section/in_active_drawer_item.dart
@@ -16,9 +16,13 @@ class InActiveDrawerItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       leading: SvgPicture.asset(drawerItemModel.image),
-      title: Text(
-        drawerItemModel.title,
-        style: AppStyles.styleMedium16(context),
+      title: FittedBox(
+          fit: BoxFit.scaleDown,
+          alignment: AlignmentDirectional.centerStart,
+        child: Text(
+          drawerItemModel.title,
+          style: AppStyles.styleMedium16(context),
+        ),
       ),
     );
   }
--- lib/features/dash_board_view/presentation/views/widgets/drawer_section/user_info/user_info_list_tile.dart
+++ lib/features/dash_board_view/presentation/views/widgets/drawer_section/user_info/user_info_list_tile.dart
@@ -4,19 +4,35 @@ import 'package:responsive_dash_board/features/dash_board_view/data/models/user_
 import 'package:responsive_dash_board/utils/app_style.dart';
 
 class UserInfoListTile extends StatelessWidget {
-  const UserInfoListTile( 
-      {super.key, required this.userInfoModel,});
+  const UserInfoListTile({
+    super.key,
+    required this.userInfoModel,
+  });
 
-  final UserInfoModel  userInfoModel;
+  final UserInfoModel userInfoModel;
   @override
   Widget build(BuildContext context) {
     return Card(
-      color:  const Color(0xFFFAFAFA),
+      color: const Color(0xFFFAFAFA),
       elevation: 0,
       child: ListTile(
         leading: SvgPicture.asset(userInfoModel.image),
-        title: Text(userInfoModel.title, style: AppStyles.styleSemiBold16(context)),
-        subtitle: Text(userInfoModel.subTitle, style: AppStyles.styleRegular12(context)),
+        title: FittedBox(
+          fit: BoxFit.scaleDown,
+          alignment: AlignmentDirectional.centerStart,
+          child: Text(
+            userInfoModel.title,
+            style: AppStyles.styleSemiBold16(context),
+          ),
+        ),
+        subtitle: FittedBox(
+            fit: BoxFit.scaleDown,
+          alignment: AlignmentDirectional.centerStart,
+          child: Text(
+            userInfoModel.subTitle,
+            style: AppStyles.styleRegular12(context),
+          ),
+        ),
       ),
     );
   }
